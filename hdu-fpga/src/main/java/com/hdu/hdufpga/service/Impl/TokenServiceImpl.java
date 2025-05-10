a928e0f532453c4106c65b7a66883ca44047f438
LiAuTraver <me@liautraver.dev>
2025-05-11 02:17:25 +0800

    (maybe) add user exp time and cnt support

--- hdu-fpga/src/main/java/com/hdu/hdufpga/service/Impl/TokenServiceImpl.java
+++ hdu-fpga/src/main/java/com/hdu/hdufpga/service/Impl/TokenServiceImpl.java
@@ -15,45 +15,56 @@ import com.hdu.hdufpga.utils.ParamUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class TokenServiceImpl implements TokenService {
-    @Resource
-    RedisUtil redisUtil;
+  @Resource
+  RedisUtil redisUtil;
 
-    @Resource
-    WaitingService waitingService;
+  @Resource
+  WaitingService waitingService;
 
-    @Override
-    public String generateToken(UserVO userVO) throws IdentifyException {
-        if (ParamUtil.CheckUserInfoLegal(userVO)) { // 检查传递的userVO参数是否为空
-            String salt = IdUtil.simpleUUID(); // 随机生成一个uuid
-            String token = ParamUtil.generateUserToken(userVO, salt); //生成token
-            if (Validator.isNull(token)) {
-                throw new IdentifyException("身份信息有误");
-            }
-            redisUtil.set(RedisConstant.REDIS_TTL_PREFIX + token, true, RedisConstant.REDIS_TTL_LIMIT, TimeUnit.SECONDS);
-            return token;
-        }
+  // fixme: this is not a good choice
+  private final ConcurrentHashMap<String, UserVO> tokTable = new ConcurrentHashMap<>();
+
+  @Override
+  public String generateToken(UserVO userVO) throws IdentifyException {
+    if (ParamUtil.CheckUserInfoLegal(userVO)) { // 检查传递的userVO参数是否为空
+      String salt = IdUtil.simpleUUID(); // 随机生成一个uuid
+      String token = ParamUtil.generateUserToken(userVO, salt); //生成token
+      if (Validator.isNull(token)) {
         throw new IdentifyException("身份信息有误");
+      }
+      redisUtil.set(RedisConstant.REDIS_TTL_PREFIX + token, true, RedisConstant.REDIS_TTL_LIMIT, TimeUnit.SECONDS);
+      redisUtil.set(RedisConstant.REDIS_EXP_START_TIME_PREFIX + token, System.currentTimeMillis(), RedisConstant.REDIS_TTL_LIMIT, TimeUnit.SECONDS);
+      tokTable.put(token, userVO);
+      return token;
     }
+    throw new IdentifyException("身份信息有误");
+  }
 
-    @Override
-    public UserConnectionVO reload(String token) throws Exception {
-        redisUtil.set(RedisConstant.REDIS_TTL_PREFIX + token, true, RedisConstant.REDIS_TTL_LIMIT, TimeUnit.SECONDS);
-        return waitingService.userInQueue(token);
-    }
+  @Override
+  public UserConnectionVO reload(String token) throws Exception {
+    redisUtil.set(RedisConstant.REDIS_TTL_PREFIX + token, true, RedisConstant.REDIS_TTL_LIMIT, TimeUnit.SECONDS);
+    return waitingService.userInQueue(token);
+  }
 
-    @Override
-    public Boolean checkToken(String token) throws NullTokenException, TokenExpiredException {
-        if (Validator.isNull(token)) {
-            throw new NullTokenException("token为空");
-        }
-        Boolean res = redisUtil.hasKey(RedisConstant.REDIS_TTL_PREFIX + token);
-        if (res) {
-            return true;
-        }
-        throw new TokenExpiredException("token已过期");
+  @Override
+  public UserVO getUserIdFromToken(String token) {
+    return tokTable.get(token);
+  }
+
+  @Override
+  public Boolean checkToken(String token) throws NullTokenException, TokenExpiredException {
+    if (Validator.isNull(token)) {
+      throw new NullTokenException("token为空");
     }
+    Boolean res = redisUtil.hasKey(RedisConstant.REDIS_TTL_PREFIX + token);
+    if (res) {
+      return true;
+    }
+    throw new TokenExpiredException("token已过期");
+  }
 }
