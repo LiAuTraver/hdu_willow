1f7afa68b71e63918afdf38d054db2d442222d8f
Zhe-xue-shou <3311356727@qq.com>
2025-07-23 13:42:39 +0800

    Base version

--- hdu-vboard/src/main/java/com/hdu/vboard/util/VirtualBoardUtil.java
+++ hdu-vboard/src/main/java/com/hdu/vboard/util/VirtualBoardUtil.java
@@ -26,7 +26,7 @@ public class VirtualBoardUtil {
     log.debug("Now to get signal from virtual board");
     if ((line = simOutput.readLine()) != null) {
       // for dedebug
-      log.debug("Recieved:"+line);
+      log.debug("Recieved:" + line);
       JSONObject signalData = JSONUtil.parseObj(line);
       JSONObject outputJson = new JSONObject();
       outputJson.set("data", signalData);
